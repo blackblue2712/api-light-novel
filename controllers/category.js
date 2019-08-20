47500fe27ee4edcc04e5827743fd7fad55c43c7c
liars <lars27399@gmail.com>
2019-08-20 11:22:30 +0700

    feature update cateogry

--- controllers/category.js
+++ controllers/category.js
@@ -57,3 +57,36 @@ module.exports.getMoreCategories = async (req, res) => {
         return res.status(200).json(categories)
     })
 }
+
+module.exports.requestRelatedCategoryId = async (req, res, next, id) => {
+    await Category.findById(id, (err, category) => {
+        if(err || !category) return res.status(400).json( {error: `Can not get category with id ${id}`} );
+        req.cateInfo = category;
+        next();
+    })
+}
+
+module.exports.getSingleCategory = (req, res) => {
+    return res.status(200).json(req.cateInfo);
+}
+
+module.exports.postUpdateCategory = (req, res) => {
+    let cate = req.cateInfo;
+    const { name, description, status } = req.body;
+    cate.name = name;
+    cate.description = description;
+    cate.status = status;
+
+    cate.save( (err, result) => {
+        if(err) return res.status(400).json( {message: `Can not update category ${name}`} );
+        return res.status(200).json( {message: `Category ${name} was updated`} );       
+    })
+}
+
+module.exports.deleteCategory = (req, res) => {
+    let category = req.cateInfo;
+    category.delete( (err,  result) => {
+        if(err) return res.status(400).json( {message: `Can not delete ${category.name}`} );
+        return res.status(200).json( {message: `Category ${category.name} was deleted`} );
+    })
+}
--- routes/category.js
+++ routes/category.js
@@ -2,15 +2,32 @@
 const express = require("express");
 const router = express.Router();
 
-const { addCategory, getCategories, getMoreCategories, getAllCategories } = require("../controllers/category");
+const { 
+    addCategory,
+    getCategories,
+    getMoreCategories,
+    getAllCategories,
+    postUpdateCategory,
+    deleteCategory,
+    requestRelatedCategoryId,
+    getSingleCategory
+} = require("../controllers/category");
 const { requireSignin, requestRelatedUserId, isAdmin } = require("../controllers/user");
 const { validateAddCategory } = require("../middlewares/index");
 
-router.post("/create/:userId",requireSignin, isAdmin, validateAddCategory, addCategory);
+router.post("/create/:userId", requireSignin, isAdmin, validateAddCategory, addCategory);
 router.get("/get/five", getCategories);
 router.get("/get/all", getAllCategories);
 router.post("/get/more", getMoreCategories);
+router.get("/get/:cateId", getSingleCategory);
 
+// Update
+router.post("/update/:cateId", requireSignin, validateAddCategory, postUpdateCategory);
 
+// Delete
+router.delete("/delete/:cateId", requireSignin, deleteCategory);
+
+// Parameters
 router.param("userId", requestRelatedUserId);
+router.param("cateId", requestRelatedCategoryId);
 module.exports = router;
